6f1dd10e3d9e1f0573024659d4c52d8db04aca71
Alex Scarlett <alex.scarlett91@gmail.com>
2018-03-22 22:09:36 -0400

    Added bootstrap and styled the game

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -30,15 +30,10 @@ $(document).ready(function() {
       img: 'assets/images/christopher-nolan.jpg'
     }),
     (questionFour = {
-      question: 'Who won the SuperBowl this year?',
+      question: 'Who LOST the 2017 SuperBowl?',
       answer: 3,
-      choices: [
-        'New York Giants',
-        'New England Patriots',
-        'Pittsburgh Steelers',
-        'Philadelphia Eagles'
-      ],
-      img: 'assets/images/eagles-superbowl.jpg'
+      choices: ['Eagles', 'Patriots', 'Steelers', 'Falcons'],
+      img: 'assets/images/falcons-logo.jpg'
     }),
     (questionFive = {
       question: 'What color IS NOT on the British flag?',
@@ -55,18 +50,13 @@ $(document).ready(function() {
         'Robert DeNiro',
         'George Clooney'
       ],
-      img: 'assets/images/sylvester-stallone-rocky.jpg'
+      img: 'assets/images/rocky-stallone.jpg'
     }),
     (questionSeven = {
-      question: 'What NBA team won the NBA Championship in 2017?',
+      question: 'What NBA team won the NBA Championship in 2016?',
       answer: 3,
-      choices: [
-        'San Antonio Spurs',
-        'Boston Celtics',
-        'Cleveland Cavaliers',
-        'Golden State Warriors'
-      ],
-      img: 'assets/images/warriors-logo.jpg'
+      choices: ['Spurs', 'Celtics', 'Warriors', 'Cavaliers'],
+      img: 'assets/images/cavs-logo.jpeg'
     }),
     (questionEight = {
       question: 'Which of these WAS NOT one of the original 13 colonies?',
@@ -91,7 +81,7 @@ $(document).ready(function() {
 
   // start game by clicking button
   $('#start-game').click(function() {
-    $('#start-game').hide();
+    $('#start-game, #subheader').hide();
     showQuestions();
   });
 
@@ -111,7 +101,7 @@ $(document).ready(function() {
         buttons.text(answers[i]);
         buttons.attr('value', answers[i]);
         buttons.attr('id', 'button' + i);
-        buttons.attr('class', 'answer');
+        buttons.attr('class', 'answer btn btn-info');
         $('#answer-choices')
           .append(buttons)
           .append('<br>');
@@ -170,7 +160,7 @@ $(document).ready(function() {
       currentQuestion++;
     } else if (questionTimer > 0) {
       questionTimer--;
-      $('#time-remaining').text(questionTimer);
+      $('#time-remaining').html(questionTimer + ' Seconds Remaining!');
     }
   };
 
@@ -182,7 +172,7 @@ $(document).ready(function() {
 
   // second interval for answer display
   var responseInterval;
-  var responseTimer = 6;
+  var responseTimer = 2;
 
   // start counter for answer display
   var startResponse = function() {
@@ -192,12 +182,11 @@ $(document).ready(function() {
   // lower response counter by 1 until 0, then show next question
   var decrementResponse = function() {
     if (responseTimer === 0) {
-      responseTimer = 6;
+      responseTimer = 2;
       $('.main-content').empty();
       showQuestions();
     } else {
       responseTimer--;
-      $('#time-remaining').text(responseTimer);
     }
   };
 
@@ -224,7 +213,7 @@ $(document).ready(function() {
   // end the game and show the final score to the player
   var endGame = function() {
     $('.main-content').empty();
-    $('#images').append(
+    $('#answer-choices').append(
       '<h2>Your Score:</h2>' +
         '<p>Questions Answered: ' +
         amtAnswered +
